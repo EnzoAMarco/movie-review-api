78fdd972254c689a99629a79c5823975105eb159
EnzoAMarco <enzom945@gmail.com>
2023-12-20 18:15:39 -0300

    inicio

---
+++ movies-review/src/main/java/dev/rendezvous/moviesreview/MoviesReviewApplication.java
@@ -0,0 +1,23 @@
+package dev.rendezvous.moviesreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MoviesReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesReviewApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String apiRoot() {
+		return "alooo, como anda weon";
+	}
+
+}
+
+// 35:30
